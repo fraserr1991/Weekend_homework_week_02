f79eb6fbb905a977c2eda408895cd15cdd6515b1
Fraser <fraser.russell1991@gmail.com>
2022-12-03 10:36:12 +0000

    Updated to query room info

--- src/room.py
+++ src/room.py
@@ -1,7 +1,8 @@
 class Room:
     def __init__(self, room_number):
         self.room_number = room_number
-        self.room_guest = []
+        self.room_guest_list = []
+        self.room_in_use_by = {}
         self.guest_in_room = False
         self.song_list = []
 
@@ -11,19 +12,23 @@ class Room:
 
     def check_in_guest(self, guest):
         if self.guest_in_room == False:
-            self.room_guest.append(guest)
+            self.room_guest_list.append(guest)
             self.guest_in_room = True
-            return self.room_guest
+            # print(self.room_guest_list)
+            return self.room_guest_list
         else:
+            print(f"Room {self.room_number} already in use by {self.room_guest_list}")
             return False
 
     def check_out_guest(self):
-        self.room_guest = []
+        self.room_guest_list = []
         self.guest_in_room = False
-        return self.room_guest
+        return self.room_guest_list
 
-    # def room_info(self):
-    #     return self.room_number
+    def room_info(self):
+        self.room_in_use_by["Room"] = self.room_number
+        self.room_in_use_by["Name"] = self.room_guest_list
+        return self.room_in_use_by
 
     # def check_in_guest(self):
     #     print(self.room.append("Bob"))
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,41 +7,44 @@ from src.guest import Guest
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.instance_of_room = Room(1)
+        self.instance_of_room_1 = Room(1)
         self.instance_of_room_2 = Room(2)
         self.instance_of_song = Song("Unholy", "Sam Smith")
+        self.instance_of_song_2 = Song("Anti-Hero", "Taylor Swift")
         self.instance_of_guest_1 = Guest("Bob", 40)
         self.instance_of_guest_2 = Guest("George", 50)
-        # self.instance_of_song_2 = Song("Anti-Hero", "Taylor Swift")
 
     def test_room_has_number(self):
-        self.assertEqual(1, self.instance_of_room.room_number)
+        self.assertEqual(1, self.instance_of_room_1.room_number)
 
     def test_add_song(self):
-        song_list = self.instance_of_room.add_song(self.instance_of_song.name_of_song)
+        song_list = self.instance_of_room_1.add_song(self.instance_of_song.name_of_song)
         self.assertEqual(["Unholy"], song_list)
     
     def test_add_guest_to_room(self):
-        guest = self.instance_of_room.check_in_guest(self.instance_of_guest_1.name)
+        guest = self.instance_of_room_1.check_in_guest(self.instance_of_guest_1.name)
         self.assertEqual(["Bob"], guest)
     
     def test_checkout_guest_from_room(self):
-        guest_check_out = self.instance_of_room.check_out_guest()
+        guest_check_out = self.instance_of_room_1.check_out_guest()
         self.assertEqual([], guest_check_out)
 
     def test_guest_in_room_is_true(self):
-        self.instance_of_room.check_in_guest(self.instance_of_guest_1.name)
-        is_guest_in_room = self.instance_of_room.check_in_guest(self.instance_of_guest_2.name)
+        self.instance_of_room_1.check_in_guest(self.instance_of_guest_1.name)
+        is_guest_in_room = self.instance_of_room_1.check_in_guest(self.instance_of_guest_2.name)
         self.assertEqual(False, is_guest_in_room)
 
     def test_guest_in_room_is_false(self):
-        is_guest_in_room = self.instance_of_room.check_in_guest(self.instance_of_guest_1.name)
+        is_guest_in_room = self.instance_of_room_1.check_in_guest(self.instance_of_guest_1.name)
         self.assertEqual(["Bob"], is_guest_in_room)
     
-    # def show_person_in_room(self):
-    #     self.instance_of_room.check_in_guest(self.instance_of_guest_1.name)
-    #     self.instance_of_room_2.check_in_guest(self.instance_of_guest_2.name)
-    #     room_info = self.instance_of_room.room_info()
+    def test_show_people_in_room(self):
+        self.instance_of_room_1.check_in_guest(self.instance_of_guest_1.name)
+        self.instance_of_room_2.check_in_guest(self.instance_of_guest_2.name)
+        room_info_1 = self.instance_of_room_1.room_info()
+        room_info_2 = self.instance_of_room_2.room_info()
+        self.assertEqual({'Room': 1, 'Name': ['Bob']}, room_info_1)
+        self.assertEqual({'Room': 2, 'Name': ['George']}, room_info_2)
 
 
     # def test_room_has_guest(self):
